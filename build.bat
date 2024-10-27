cec405f5f3beb76b8d60bcc1019c1d71b20d854c
Diego Jesus <diego.a.r.js@gmail.com>
2024-10-27 10:03:39 +0000

    Add conan profiles
    magic_enum requires a cppstd of 17 or higher but, by default, conan
    profile detect was creating a profile with cppstd 14. To fix this, a new
    set of conan profiles are added.

--- build.bat
+++ build.bat
@@ -3,25 +3,34 @@
 set aswpp_ROOT=%1%
 if "%~1" == "" (set aswpp_ROOT=%cd%)
 
+set CONAN_PROFILE=%2%
+if "%~2" == "" (set CONAN_PROFILE="%aswpp_ROOT%/tools/profiles/win_Rel.txt")
+
 set BUILD_ROOT=%aswpp_ROOT%\build
 
 echo "########################################"
 echo "Running build.sh"
-echo "  aswpp root: %aswpp_ROOT%"
-echo "   build root: %BUILD_ROOT%"
+echo "    aswpp root: %aswpp_ROOT%"
+echo "    build root: %BUILD_ROOT%"
+echo " conan profile: %CONAN_PROFILE%"
 echo "########################################"
 
 REM ----------------------------------------
 REM  install conan dependencies
 REM ----------------------------------------
 git -C %aswpp_ROOT% submodule update --init --recursive
-conan profile detect
 conan install                                     ^
   -of %BUILD_ROOT%                                ^
   %aswpp_ROOT%                                    ^
+  --profile:host=%CONAN_PROFILE%                  ^
+  --profile:build=%CONAN_PROFILE%                 ^
   --build=missing
 
 REM ----------------------------------------
 REM  Build aswpp
 REM ----------------------------------------
-conan build -of %BUILD_ROOT% %aswpp_ROOT%
+conan build                                       ^
+  -of %BUILD_ROOT%                                ^
+  %aswpp_ROOT%                                    ^
+  --profile:host=%CONAN_PROFILE%                  ^
+  --profile:build=%CONAN_PROFILE%                 ^
--- build.sh
+++ build.sh
@@ -4,26 +4,48 @@ aswpp_ROOT=${1}
 if [ -z ${aswpp_ROOT} ]; then
   aswpp_ROOT=$(pwd)
 fi
-
 BUILD_ROOT=${aswpp_ROOT}/build
+
+#----------------------------------------
+# Detect OS
+# Used to specify the conan profiles
+#----------------------------------------
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     DETECTED_OS=linux;;
+    Darwin*)    DETECTED_OS=mac;;
+    *)          DETECTED_OS="UNKNOWN:${unameOut}"
+esac
+
+CONAN_PROFILE=${2}
+if [ -z ${CONAN_PROFILE} ]; then
+  CONAN_PROFILE="${aswpp_ROOT}/tools/profiles/${DETECTED_OS}_Rel.txt"
+fi
 
 echo "########################################"
 echo "Running build.sh"
-echo "   aswpp root: ${aswpp_ROOT}"
-echo "   build root: ${BUILD_ROOT}"
+echo "            OS: ${DETECTED_OS}"
+echo "    aswpp root: ${aswpp_ROOT}"
+echo "    build root: ${BUILD_ROOT}"
+echo " conan profile: ${CONAN_PROFILE}"
 echo "########################################"
 
 #----------------------------------------
 # install conan dependencies
 #----------------------------------------
 git -C ${aswpp_ROOT} submodule update --init --recursive
-conan profile detect
 conan install                                     \
   -of ${BUILD_ROOT}                               \
   ${aswpp_ROOT}                                   \
+  --profile:host=${CONAN_PROFILE}                 \
+  --profile:build=${CONAN_PROFILE}                \
   --build=missing
 
 #----------------------------------------
 # Build aswpp
 #----------------------------------------
-conan build -of ${BUILD_ROOT} ${aswpp_ROOT}
+conan build                                       \
+  -of ${BUILD_ROOT}                               \
+  ${aswpp_ROOT}                                   \
+  --profile:host=${CONAN_PROFILE}                 \
+  --profile:build=${CONAN_PROFILE}
